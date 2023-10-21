e340f3eebc3175d6a9705ec72b2bd1941746b5d7
Antony Sajesh <antonysajesh321@gmail.com>
2023-10-21 14:48:17 -0500

    git push

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-import { Configuration, OpenAIApi } from 'openai'
+/*import { Configuration, OpenAIApi } from 'openai'
 
 
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
 })
 
-const openai = new OpenAIApi(configuration)
+const openai = new OpenAIApi(configuration)*/
 
 const chatbotConversation = document.getElementById('chatbot-convo')
  
@@ -47,13 +47,29 @@ document.addEventListener('submit', (e) => {
 })
 
 async function fetchReply(){
-    const response = await openai.createChatCompletion({
+    const url = 'https://resplendent-paletas-668c50.netlify.app/.netlify/functions/fetchAI'
+
+    const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+        'content-type': 'text/plain',
+    },
+     body: conversationArr
+   })
+   const data = await response.json()
+   console.log(data)
+
+
+
+
+
+    /*const response = await openai.createChatCompletion({
         model: 'gpt-4',
         messages: conversationArr,
-        presence_penalty: 0
+        presence_penalty: 0 
     }) 
-    conversationArr.push(response.data.choices[0].message)
-    renderTypewriterText(response.data.choices[0].message.content)
+    //conversationArr.push(response.data.choices[0].message)
+    //renderTypewriterText(response.data.choices[0].message.content)*/
 }
 
 function renderTypewriterText(text) {
