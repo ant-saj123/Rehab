f8647eb89892ea4974b7ad74a8c4087ddb58968c
Antony Sajesh <antonysajesh321@gmail.com>
2023-10-21 15:23:09 -0500

    git commit

--- netlify/functions/fetchAI/fetchAI.js
+++ netlify/functions/fetchAI/fetchAI.js
@@ -1,30 +1,33 @@
-import { Configuration, OpenAIApi } from 'openai'
-
+import { Configuration, OpenAIApi } from 'openai';
 
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
-})
+});
 
-const openai = new OpenAIApi(configuration)
+const openai = new OpenAIApi(configuration);
+
 const handler = async (event) => {
-  try {
-    const response = await openai.createChatCompletion({
-      model: 'gpt-4',
-      messages: event.body,
-      presence_penalty: 0 
-  }) 
-    return {
-      statusCode: 200,
-      body: JSON.stringify({
+    try {
+        const response = await openai.createChatCompletion({
+            model: 'gpt-4',
+            messages: event.body,
+            presence_penalty: 0 
+        });
 
-        reply: response.data 
-      }),
-     
+        return {
+            statusCode: 200,
+            headers: {
+                'Access-Control-Allow-Origin': 'https://rehabeviv.netlify.app',
+                'Access-Control-Allow-Headers': 'Content-Type',
+            },
+            body: JSON.stringify({
+                reply: response.data,
+            }),
+        };
+    } catch (error) {
+        console.error('Error:', error);
+        return { statusCode: 500, body: error.toString() };
     }
-  } catch (error) {
-    console.error('Error:', error);
-    return { statusCode: 500, body: error.toString() }
-  }
-}
+};
 
-module.exports = { handler }
+module.exports = { handler };
